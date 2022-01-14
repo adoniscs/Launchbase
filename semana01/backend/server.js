eeb9933fc2ae983dd77725eecd729e8005d09d26
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-14 11:46:40 -0300

    Started backend and change in template engine

---
+++ semana01/backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const server = express()
+// const path = require('path')
+const PORT = 3000
+
+// user arquivos estáticos (img, css)
+server.use(express.static('public'))
+
+// setar o motor de vizualização - template engine
+server.set('view engine', 'njk')
+nunjucks.configure('views', {
+  express: server
+})
+
+server.get('/', (request, response) => {
+  return response.render('index')
+})
+
+server.get('/classes', (request, response) => {
+  return response.render('classes')
+})
+
+server.get('/classes', (request, response) => {
+  return response.render('classes')
+})
+
+server.listen(PORT, () => {
+  console.log('Server is running...')
+})
