7210ba00d483c64cc084e5a91a5d2eb92dc1c55d
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-13 22:17:01 -0300

    Recipes page and conpleted modal

---
+++ semana01/foodfy/assets/js/script.js
@@ -0,0 +1,23 @@
+const $modalOverlay = document.querySelector('.modal-overlay')
+const $closeModal = document.querySelector('.close-modal')
+const $cards = document.querySelectorAll('.card')
+
+for (let $card of $cards) {
+  $card.addEventListener('click', function () {
+    $modalOverlay.classList.add('active')
+
+    const image = $card.getAttribute('id')
+    $modalOverlay.querySelector('img').src = `./assets/img/${image}`
+
+    // =============================== // ESSE CÓDIGO PRECISA SER REFATORADO // =============================== //
+    $modalOverlay.querySelector('p').innerHTML =
+      $card.querySelector('p').textContent
+    $modalOverlay.querySelector('span').innerHTML =
+      $card.querySelector('span').textContent
+    
+  })
+}
+
+$closeModal.addEventListener('click', function () {
+  $modalOverlay.classList.remove('active')
+})
