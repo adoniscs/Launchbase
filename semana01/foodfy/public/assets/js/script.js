577e5483e74aca5b013216f86dc9bbce1ffb7183
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-16 21:18:43 -0300

    Remove modal

--- semana01/foodfy/public/assets/js/script.js
+++ semana01/foodfy/public/assets/js/script.js
@@ -1,5 +1,3 @@
-const $modalOverlay = document.querySelector('.modal-overlay');
-const $closeModal = document.querySelector('.close-modal');
 const $cards = document.querySelectorAll('.card');
 
 for (let $card of $cards) {
@@ -8,15 +6,5 @@ for (let $card of $cards) {
 
     const image = $card.getAttribute('id');
     $modalOverlay.querySelector('img').src = `/${image}`;
-
-    // =============================== // ESSE CÓDIGO PRECISA SER REFATORADO // =============================== //
-    $modalOverlay.querySelector('p').innerHTML =
-      $card.querySelector('p').textContent;
-    $modalOverlay.querySelector('span').innerHTML =
-      $card.querySelector('span').textContent;
   });
 }
-
-$closeModal.addEventListener('click', function () {
-  $modalOverlay.classList.remove('active');
-});
