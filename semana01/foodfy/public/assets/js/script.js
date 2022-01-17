614ff8b33bedb383c5c1ed28aaa0ade9878f3cf1
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-17 19:28:12 -0300

    Foodfy challenge finished

--- semana01/foodfy/public/assets/js/script.js
+++ semana01/foodfy/public/assets/js/script.js
@@ -1,5 +1,15 @@
 const $cards = document.querySelectorAll('.card');
 
+const $btnIngredients = document.querySelector('#btnIngredients');
+const $btnPreparation = document.querySelector('#btnPreparation');
+const $btnInformation = document.querySelector('#btnInformation');
+
+const $recipeIngredient = document.querySelector('.recipe__ingredients-items');
+const $recipePreparation = document.querySelector('.recipe__preparation-items');
+const $recipeInformation = document.querySelector(
+  '.recipe__information-paragraph'
+);
+
 for (let $card of $cards) {
   $card.addEventListener('click', function () {
     const recipeId = $card.getAttribute('id');
@@ -7,3 +17,33 @@ for (let $card of $cards) {
     window.location.href = `/recipe/${recipeId}`;
   });
 }
+
+$btnIngredients.addEventListener('click', function () {
+  if ($btnIngredients.innerHTML === 'esconder') {
+    $recipeIngredient.classList.add('hiden');
+    $btnIngredients.innerHTML = 'mostrar';
+  } else {
+    $recipeIngredient.classList.remove('hiden');
+    $btnIngredients.innerHTML = 'esconder';
+  }
+});
+
+$btnPreparation.addEventListener('click', function () {
+  if ($btnPreparation.innerHTML === 'esconder') {
+    $recipePreparation.classList.add('hiden');
+    $btnPreparation.innerHTML = 'mostrar';
+  } else {
+    $recipePreparation.classList.remove('hiden');
+    $btnPreparation.innerHTML = 'esconder';
+  }
+});
+
+$btnInformation.addEventListener('click', function () {
+  if ($btnInformation.innerHTML === 'esconder') {
+    $recipeInformation.classList.add('hiden');
+    $btnInformation.innerHTML = 'mostrar';
+  } else {
+    $recipeInformation.classList.remove('hiden');
+    $btnInformation.innerHTML = 'esconder';
+  }
+});
