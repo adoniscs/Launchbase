9088a40dd1a3dd3e2edd495e567c351125b9f4be
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-18 19:46:31 -0300

    Challenge 4-1: header

---
+++ semana02/desafios/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const njk = require('nunjucks');
+const routes = require('./routes');
+const server = express();
+const PORT = 5000;
+
+server.use(express.static('public'));
+server.use(routes);
+
+server.set('view engine', 'njk');
+njk.configure('views', {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+
+server.listen(PORT, () => {
+  return console.log('Server up on port: ', PORT);
+});
