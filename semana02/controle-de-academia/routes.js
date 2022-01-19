e542ce804f1176979ba4d71fe901d115e564bc2d
Adonis Cipriano <desenvolvimento777@gmail.com>
2022-01-19 20:28:36 -0300

    validating the data in the backend

--- semana02/controle-de-academia/routes.js
+++ semana02/controle-de-academia/routes.js
@@ -14,8 +14,18 @@ routes.get('/instructors/create', (req, res) => {
 });
 
 routes.post('/instructors', (req, res) => {
+  // tornar o preenchimento dos campos do form
+  // obrigatorios
+  const keys = Object.keys(req.body); // retorna um array com as chaves do objeto
 
-  return res.send(req.body);
+  for (let key of keys) {
+    // mesma coisa que: req.body.avatar_url == '' etc...
+    if (req.body[key] == '') {
+      return res.send('Please, fill all fields!');
+    }
+  }
+
+  return res.send(keys);
 });
 
 routes.get('/members', (req, res) => {
